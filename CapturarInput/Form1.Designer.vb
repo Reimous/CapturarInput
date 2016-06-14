491f918c498568b3b1ca0ca3faefb0b008351e7e
Reimous <victor_r88@hotmail.com>
2016-06-15 00:27:32 +0200

    Añadido proyecto
    1

---
+++ CapturarInput/Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btn_play = New System.Windows.Forms.Button()
+        Me.btn_grabar = New System.Windows.Forms.Button()
+        Me.btn_stop = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(44, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'btn_play
+        '
+        Me.btn_play.Location = New System.Drawing.Point(368, 188)
+        Me.btn_play.Name = "btn_play"
+        Me.btn_play.Size = New System.Drawing.Size(75, 23)
+        Me.btn_play.TabIndex = 1
+        Me.btn_play.Text = "Play"
+        Me.btn_play.UseVisualStyleBackColor = True
+        '
+        'btn_grabar
+        '
+        Me.btn_grabar.Location = New System.Drawing.Point(206, 188)
+        Me.btn_grabar.Name = "btn_grabar"
+        Me.btn_grabar.Size = New System.Drawing.Size(75, 23)
+        Me.btn_grabar.TabIndex = 3
+        Me.btn_grabar.Text = "REC"
+        Me.btn_grabar.UseVisualStyleBackColor = True
+        '
+        'btn_stop
+        '
+        Me.btn_stop.Location = New System.Drawing.Point(287, 188)
+        Me.btn_stop.Name = "btn_stop"
+        Me.btn_stop.Size = New System.Drawing.Size(75, 23)
+        Me.btn_stop.TabIndex = 3
+        Me.btn_stop.Text = "Stop"
+        Me.btn_stop.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(672, 463)
+        Me.Controls.Add(Me.btn_stop)
+        Me.Controls.Add(Me.btn_grabar)
+        Me.Controls.Add(Me.btn_play)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btn_play As System.Windows.Forms.Button
+    Friend WithEvents btn_grabar As System.Windows.Forms.Button
+    Friend WithEvents btn_stop As System.Windows.Forms.Button
+
+End Class
---
+++ CapturarInput/Form1.vb
@@ -0,0 +1,122 @@
+﻿Public Class Form1
+    Private timer As Stopwatch
+    Private sw As Stopwatch
+    Private EnableInput, playing, recording As Boolean
+    Private ListaInputs As List(Of Inputs)
+
+    Public Declare Function GetAsyncKeyState Lib "user32.dll" (ByVal vKey As Int32) As UShort
+    Private Declare Sub mouse_event Lib "user32" (ByVal dwFlags As Integer,
+      ByVal dx As Integer, ByVal dy As Integer, ByVal cButtons As Integer,
+      ByVal dwExtraInfo As Integer)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        timer = New Stopwatch
+        sw = New Stopwatch
+        Timer1.Interval = 25
+        timer.Start()
+        Timer1.Start()
+        EnableInput = True
+        playing = False
+        recording = False
+
+        ListaInputs = New List(Of Inputs)
+
+        Label1.Text = ""
+    End Sub
+
+    Private Function Cronometro(ByVal tiempo As Integer) As Boolean
+
+        If sw.ElapsedMilliseconds > tiempo Then
+            sw.Restart()
+            Return True
+        Else
+            sw.Start()
+            Return False
+        End If
+    End Function
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        'If grabando Then
+
+        If recording Then
+            For i = 0 To 255
+                If GetAsyncKeyState(i) Then
+                    If EnableInput = False Then
+                        EnableInput = Cronometro(100)
+                    Else
+                        EnableInput = False
+                        Label1.Text &= Chr(i) & "_" & timer.ElapsedMilliseconds & "_" & Cursor.Position.X & "_" & Cursor.Position.Y
+                        Dim inp As New Inputs
+                        inp.inp = i
+                        inp.tiempo = timer.ElapsedMilliseconds
+                        inp.posMouse = New Point(Cursor.Position.X, Cursor.Position.Y)
+                        ListaInputs.Add(inp)
+                    End If
+
+                End If
+            Next
+        End If
+
+        If GetAsyncKeyState(Convert.ToInt32(Keys.Escape)) Then
+
+        End If
+
+        If GetAsyncKeyState(Convert.ToInt32(Keys.A)) Then
+
+        End If
+
+        If GetAsyncKeyState(1) Then
+        ElseIf GetAsyncKeyState(2) Then
+
+        End If
+        If GetAsyncKeyState(Keys.ShiftKey) Then
+            recording = Not recording
+        End If
+        'End If
+    End Sub
+
+    Private Sub btn_play_Click(sender As Object, e As EventArgs) Handles btn_play.Click
+        play()
+    End Sub
+
+    Private Sub play()
+        Dim timerPlay As New Stopwatch
+        timerPlay.Start()
+        Dim count = 0
+        playing = True
+        While count < ListaInputs.Count
+            If ListaInputs(count).tiempo < timerPlay.ElapsedMilliseconds Then
+                If Not playing Then
+                    Exit While
+                End If
+                If ListaInputs(count).inp = 1 Then
+                    Cursor.Position = New Point(ListaInputs(count).posMouse.X, ListaInputs(count).posMouse.Y)
+                    mouse_event(&H2, 0, 0, 0, 0)
+                    mouse_event(&H4, 0, 0, 0, 0)
+                ElseIf ListaInputs(count).inp = 2 Then
+                Else
+                    SendKeys.Send(Chr(ListaInputs(count).inp))
+                End If
+
+                count += 1
+            End If
+
+        End While
+    End Sub
+
+    Private Sub btn_grabar_Click(sender As Object, e As EventArgs) Handles btn_grabar.Click
+        recording = True
+    End Sub
+
+    Private Sub btn_stop_Click(sender As Object, e As EventArgs) Handles btn_stop.Click
+        playing = False
+        recording = False
+    End Sub
+End Class
+
+Public Class Inputs
+    Public inp As Integer
+    Public tiempo As Integer
+    Public posMouse As Point
+End Class
+
